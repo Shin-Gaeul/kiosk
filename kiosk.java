065bd10492b71ec279e73aa6399e0a34a8ba23b0
t2023-m0014 <qa9377@gmail.com>
2024-11-22 17:10:46 +0900

    첫 커밋

--- kiosk.java
+++ kiosk.java
@@ -2,12 +2,43 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+public class kiosk {
+
+    //menuitem을 관리하는 리스트
+    List<MenuItem> menuItems = new ArrayList<>();
+    Scanner sc = new Scanner(System.in);
+
+    public void start() {
+        System.out.println("=== 키오스크 프로그램 ===");
+
+        while (true) {
+            System.out.println("1. 메뉴 추가");
+            System.out.println("2. 메뉴 보기");
+            System.out.println("0. 종료");
+            System.out.print("선택: ");
+
+            int choice = sc.nextInt();  // 사용자 입력
+            sc.nextLine();
+
+            switch (choice) {
+                case 1:
+                    addMenu();
+                    break;
+                case 2:
+                    printMenu();
+                    break;
+                case 0:
+                    System.out.println("프로그램을 종료합니다.");
+                    return;  // 프로그램 종료
+                default:
+                    System.out.println("올바른 번호를 선택하세요.");
+            }
+        }
+    }
+
+
+    public void addMenu() {
         List<MenuItem> menuItems = new ArrayList<>();
-        Scanner sc = new Scanner(System.in);
-
-
         while (true) {
             System.out.println("이름을 입력하세요: ");
             String name = sc.nextLine();
@@ -25,9 +56,11 @@ public class Main {
                 MenuItem menuItem = new MenuItem(name, price, explane);
                 menuItems.add(menuItem);
             }
+
         }
+    }
 
-
+    public void printMenu() {
         String a = "0. 종료          | 종료";
 
         for (int i = 0; i < menuItems.size(); i++) {
@@ -47,4 +80,3 @@ public class Main {
     }
 
 }
-
